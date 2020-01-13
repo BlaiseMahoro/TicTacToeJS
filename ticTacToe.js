be3b1910f4a5193c839c0b4ee58004938732b013
Blaise Mahoro <Blaise Mahoro@BLAISEPC>
2020-01-13 12:27:51 -0600

    Added a feature to play again

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,165 +1,26 @@
-
+//loads the standard input library
 const rl = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   })
 
+
 function initBoard(board){
+  //resets the board
     for(var i =0; i<9; i++){
         board[i]=' ';
     }
 
 }
-const game= async()=>{
- var board = [9];
- 
- initBoard(board);
- //dispBoard(board);
- var players = ['X', 'O'];
- var playerNames= new Array(2);
- var player1, player2;
- var turn = 'X';
- 
- var mode = await new Promise((resolve, reject) => {
-    rl.question('Choose mode Computer(C) and Human(H) : ', (mode) => {
-   resolve(mode.trim())})
-});
-while(mode.toUpperCase()!=='C' &&mode.toUpperCase()!=='H' ){
-  console.log('Try again! Enter C or H.')
-  var mode = await new Promise((resolve, reject) => {
-    rl.question('Choose mode Computer(C) and Human(H) : ', (mode) => {
-   resolve(mode.trim())})
-});
-}
-if(mode.toUpperCase() =='H'){
-  player1 = await new Promise((resolve, reject) => {
-  rl.question(`Enter Player X name (optional): `, (input) => {
-  resolve(input.trim())})
-  });
 
-  player2 = await new Promise((resolve, reject) => {
-    rl.question(`Enter Player O name (optional): `, (input) => {
-    resolve(input.trim())})
-    });
-  
 
-}
-else{
-  player1 = 'ComputerAI'
-  console.log('Player X is ', player1)
-  player2 = await new Promise((resolve, reject) => {
-    rl.question(`Enter Player O name (optional): `, (input) => {
-    resolve(input.trim())})
-    });
-  
-  
-}
- 
-
-while(!gameOver(board)){
-  if (turn==='X'){
-    //X play
-
-    if(mode.toUpperCase()==='C'){
-      computerPlay(board, 'X');
-      console.log('ComputerAI played!')
-    }
-    else{
-      var pos = await new Promise((resolve, reject) => {
-        rl.question(player1?player1+', enter a position (0-9) : ':'Player1, enter a position(0-9): ', (input) => {
-       resolve(input)})
-      
-    });
-    while(board[pos]!==' '||parseInt(pos) <0 ||parseInt(pos) >8){
-      console.log('Spot is already taken or invalid position entered!')
-      var pos = await new Promise((resolve, reject) => {
-        rl.question(player1?player1+', enter a position (0-9) : ':'Player1, enter a position(0-9): ', (input) => {
-       resolve(input)})
-      
-    });
-    
-    }
-    board[pos] = 'X';
-    
-    }
-    //dispBoard(board)
-    turn = 'O';
-  }
-  else{
-    
-    var pos = await new Promise((resolve, reject) => {
-      rl.question(player2?player2+', enter a position (0-9) : ':'Player2, enter a position(0-9): ', (input) => {
-     resolve(input)})
-    
-  });
-  while(board[pos]!==' '||parseInt(pos) <0 ||parseInt(pos) >8){
-    console.log('Spot is already taken or invalid position entered!')
-    var pos = await new Promise((resolve, reject) => {
-      rl.question(player2?player2+', enter a position (0-9) : ':'Player2, enter a position(0-9): ', (input) => {
-     resolve(input)})
-    
-  });
- 
-  }
-  board[pos] = 'O';
-  
-  
-    turn ='X';
-  }
-  dispBoard(board)
-
-}
-
-rl.close();
-console.log('Game over!')
-if(isWinner(board, 'X')){
-  console.log(player1?player1:'Player1'," wins!")
-}
-else if(isWinner(board,'O')){
-  console.log(player2?player2:'Player2', " wins!")
-}
-else
-  console.log(`It's a draw!`)
-// console.log(board[2], board[5],board[8])
-// console.log(board[2]+board[5]+board[8]==='O'.repeat(3))
-
-//  while(!gameOver(board)){
-
- 
-//  }
-}
-function computerVHumanMode(playerName, board){
-    turn = 'X';
-    while(!gameOver(board)){
-    if(turn=='X')
-        computerPlay(board, 'X')
-  else{
-    //   var pos = await new Promise((resolve, reject) => {
-    //     rl.question(player+', enter a position (0-9) : ', (input) => {
-    //    resolve(input)})
-      
-    // });
-    while(board[pos]===' '||Number.isInteger(parseInt(pos))||parseInt(pos) <0 ||parseInt(pos) >8){ 
-      console.log('Spot is already taken or invalid position is entered!')
-    //   var pos = await new Promise((resolve, reject) => {
-    //     rl.question(playerName?playerName+', enter a position (0-9) : ':'Player2, enter a position(0-9): ', (input) => {
-    //    resolve(input)})
-      
-    // });
-    }
-  }
-    turn = 'O'
-}
-}
-function humanVHuman(play1,play2, board){
-  
-}
 
 function gameOver(board){
-    //check if game is over or there is winner
+    //check if board is full or there is winner
     return isFull(board)||isWinner(board,'X')||isWinner(board,'O');
 }
 function isFull(board){
+  //checks if board is full
   for(i=0;i<9;i++){
     if(board[i]===' '){
       return false;
@@ -170,19 +31,15 @@ function isFull(board){
 
 }
 function isWinner(board, mark){
+  //checks if the mark(X,O) passed is the winner
   for(i=0; i<9;i++){
     return board[0]+board[1]+board[2]===mark.repeat(3) ||board[0]+board[3]+board[6]===mark.repeat(3) ||board[0]+board[4]+board[8]===mark.repeat(3) ||
     board[3]+board[4]+board[5]===mark.repeat(3) ||board[6]+board[7]+board[8]===mark.repeat(3) ||board[1]+board[4]+board[7]===mark.repeat(3) ||
     board[2]+board[5]+board[8]===mark.repeat(3) ||board[2]+board[4]+board[6]===mark.repeat(3) ;
   }
 }
-// function markBoard(board, pos, mark){
-//    if (board[pos] !== " "){
-//       throw Error('spot already taken!')
-//    }
-//    board[pos] = mark
-// }
 function computerPlay(board, mark){
+  //looks for an empty spot and plays in that spot
    for(i=0;i<9;i++){
      if(board[i]===' '){
        board[i]=mark;
@@ -192,10 +49,155 @@ function computerPlay(board, mark){
 }
 
 function dispBoard(board){
-    console.log(board[0]+'|'+board[1]+'|'+board[2]+'\n'+
-    '_____'+'\n'+
-    board[3]+'|'+board[4]+'|'+board[5]+'\n'+
-    '_____'+'\n'+
-    board[6]+'|'+board[7]+'|'+board[8]+'\n');
+  //displays the current board 
+  console.log(board[0]+'|'+board[1]+'|'+board[2]+'\n'+
+  '_____'+'\n'+
+  board[3]+'|'+board[4]+'|'+board[5]+'\n'+
+  '_____'+'\n'+
+  board[6]+'|'+board[7]+'|'+board[8]+'\n');
 }
+
+
+
+const game= async()=>{
+  //this is where the game is played.Takes care of the inputs and outputs.
+  
+   var board = [9];//declares an array that will represent the board
+  
+   var play_again = 'Y';//initially play_again is set to Y to allow playing
+  
+   while(play_again==='Y'){
+   initBoard(board);//resets board
+   
+   var player1, player2;
+   var turn = 'X';//by deault player1 is X and player2 is O
+   
+   var mode = await new Promise((resolve, reject) => {
+      rl.question('Choose mode Computer(C) and Human(H) : ', (input) => {
+     resolve(input.trim())})
+  });
+  //validates user input
+  while(mode.toUpperCase()!=='C' &&mode.toUpperCase()!=='H' ){
+    console.log('Try again! Enter C or H.')
+    var mode = await new Promise((resolve, reject) => {
+      rl.question('Choose mode Computer(C) and Human(H) : ', (input) => {
+     resolve(input.trim())})
+    });
+  }
+  if(mode.toUpperCase() =='H'){
+    //prompts for player1's name
+    player1 = await new Promise((resolve, reject) => {
+    rl.question(`Enter Player X's name (optional): `, (input) => {
+    resolve(input.trim())})
+    });
+    //prompts for player2's name
+    player2 = await new Promise((resolve, reject) => {
+      rl.question(`Enter Player O's name (optional): `, (input) => {
+      resolve(input.trim())})
+      });
+    
+  
+  }
+  else{
+    //By default computerAI is X in computer mode
+    player1 = 'ComputerAI'
+    console.log('Player X is ', player1)
+    player2 = await new Promise((resolve, reject) => {
+      rl.question(`Enter Player O's name (optional): `, (input) => {
+      resolve(input.trim())})
+      });
+    
+    
+  }
+   
+  while(!gameOver(board)){
+    //this loops allows checking if there is a winner or board is full.
+    if (turn==='X'){
+      //player X play
+  
+      if(mode.toUpperCase()==='C'){
+        computerPlay(board, 'X');
+        console.log('ComputerAI played!')
+      }
+      else{
+        var pos = await new Promise((resolve, reject) => {
+          rl.question(player1?player1+', enter a position (0-8) : ':'Player1, enter a position(0-8): ', (input) => {
+         resolve(input)})
+        
+      });
+      while(board[pos]!==' '||parseInt(pos) <0 ||parseInt(pos) >8){
+        console.log('Spot is already taken or invalid position entered!')
+        var pos = await new Promise((resolve, reject) => {
+          rl.question(player1?player1+', enter a position (0-8) : ':'Player1, enter a position(0-8): ', (input) => {
+         resolve(input)})
+        
+      });
+      
+      }
+      board[pos] = 'X';
+      
+      }
+      //change turn 
+      turn = 'O';
+    }
+    else{
+      //playerO plays 
+      var pos = await new Promise((resolve, reject) => {
+        rl.question(player2?player2+', enter a position (0-8) : ':'Player2, enter a position(0-8): ', (input) => {
+       resolve(input)})
+      
+    });
+    //validates user input  
+    while(board[pos]!==' '||parseInt(pos) <0 ||parseInt(pos) >8){
+      console.log('Spot is already taken or invalid position entered!')
+      var pos = await new Promise((resolve, reject) => {
+        rl.question(player2?player2+', enter a position (0-8) : ':'Player2, enter a position(0-8): ', (input) => {
+       resolve(input)})
+      
+    });
+   
+    }
+    board[pos] = 'O';
+    
+    
+      turn ='X';
+    }
+    dispBoard(board)
+  
+  }
+  
+  console.log('Game over!')
+  
+  //determines and displays winner.
+  if(isWinner(board, 'X')){
+    console.log(player1?player1:'Player1'," wins!")
+  }
+  else if(isWinner(board,'O')){
+    console.log(player2?player2:'Player2', " wins!")
+  }
+  else
+    console.log(`It's a draw!`)
+  
+  //asks the user whether they want to continue palying
+  play_again = await new Promise((resolve,reject)=>{
+    rl.question('Do you want to play another game? (N)o or (Y)es: ', (input)=>{
+      resolve(input)
+    })
+  })
+  //looking for the first character
+  play_again=play_again.trim()[0].toLocaleUpperCase()
+  //validates input
+  while( play_again != 'N'&& play_again!='Y'){
+    console.log('Invalid answer. \n')
+    play_again = await new Promise((resolve,reject)=>{
+      rl.question('Do you want to play another game? (N)o or (Y)es: ', (input)=>{
+        resolve(input)
+      })
+    })
+    play_again=play_again.trim()[0].toLocaleUpperCase()
+  }
+  }
+  rl.close();
+  console.log('Good bye!')
+  }
 game();
